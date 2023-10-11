2414bcdb807c372610b5d08a998bcf0fb1fdbaee
Geoff Pack <geoff@dhillon-pack.net>
2023-10-12 02:00:06 +1100

    PWA test — take 2

--- worker.js
+++ worker.js
@@ -1,27 +1,27 @@
-const VERSION = "v0.2";
-
-const CACHE_NAME = `SunClock-${VERSION}`;
+const VERSION = "20231012T0154";
+const CACHE_NAME = `DegreesWhat-${VERSION}`;
 
 const APP_RESOURCES = [
 	"./",
 	"index.html",
-	"main.js",
-	"data.js",
 	"main.css",
+	"main.js",
+	"data.js"
 ];
 
 // Install: cache the static resources
 self.addEventListener("install", (event) => {
-	event.waitUntil(
-		(async () => {
-			const cache = await caches.open(CACHE_NAME);
-			cache.addAll(APP_RESOURCES);
-		})(),
-	);
+	console.log("Service worker install event", event);
+	event.waitUntil(async () => {
+		const cache = await caches.open(CACHE_NAME);
+		return cache.addAll(APP_RESOURCES);
+	});
 });
+
 
 // Activate: delete old caches
 self.addEventListener("activate", (event) => {
+	console.log("Service worker activate event", event);
 	event.waitUntil(
 		(async () => {
 			const names = await caches.keys();
@@ -37,25 +37,15 @@ self.addEventListener("activate", (event) => {
 	);
 });
 
-// Fetch: intercept server requests and get from cache instead of network
-self.addEventListener("fetch", (event) => {
-	// As a single page app, direct app to always go to cached home page.
-	if (event.request.mode === "navigate") {
-		event.respondWith(caches.match("./"));
-		return;
-	}
 
-	// For all other requests, go to the cache first, and then the network.
+// Fetch: network first, cache fallback
+self.addEventListener("fetch", event => {
+	//console.log("Service worker fetch event", event);
+	console.log(`URL requested: ${event.request.url}`);
 	event.respondWith(
-		(async () => {
-			const cache = await caches.open(CACHE_NAME);
-			const cachedResponse = await cache.match(event.request.url);
-			if (cachedResponse) {
-				// Return the cached response if it's available.
-				return cachedResponse;
-			}
-			// If resource isn't in the cache, return a 404.
-			return new Response(null, { status: 404 });
-		})(),
+		fetch(event.request)
+		.catch(error => {
+			return caches.match(event.request) ;
+		})
 	);
 });
